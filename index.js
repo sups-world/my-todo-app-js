444234f79d291a325902fc0e50e4fade9eac8b1b
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-29 14:42:14 +0545

    able to use local storage now

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const listContainerEl = document.getElementById("display-tasks")
 listContainerEl.addEventListener("click",(e) => {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }else if(e.target.tagName ==="SPAN"){
       e.target.parentElement.remove();
+      saveData();
     }
 },false)
 
@@ -22,8 +24,22 @@ function addTask(){
     // to show cross sign, we can show mulitplication sign as follows
     let spanItem = document.createElement("span");
     spanItem.innerHTML="\u00d7";
-    list.appendChild(spanItem)
+    list.appendChild(spanItem);
 
+
+    saveData();
     inputEl.value = '';
   }
 }
+
+function saveData() {
+  localStorage.setItem("data",listContainerEl.innerHTML)
+}
+
+function showData(){
+  listContainerEl.innerHTML = localStorage.getItem("data");
+}
+
+
+// to show data from local storage when website is closed and re-opened
+showData();
